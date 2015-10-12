ea729290ec23346390728ea0b980b6f71fe7fe4f
Able <Able@AbleMachine>
2015-10-12 13:40:39 -0400

    Initial commit

---
+++ Test_Simulation_and_Dice_Game/Test.java
@@ -0,0 +1,39 @@
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	public void simulateQuestion(){
+	
+		System.out.println("Do contructors return a value?");	
+		System.out.println("How many return a value(s) are possible from a method?");
+		
+	}
+	
+	public void checkAnswer(){
+		
+	}
+	
+	public void inputAnswer(){
+		
+	}
+	
+	public void outputResuts(){
+		switch ( randomObject.nextInt(4) )
+		{
+		case 0:
+		return("Very good!");
+	
+		}
+
+	}
+	
+	
+	
+	
+
+}
